5a9fd7c4370aecdb363aa42f90ecf60aedd1b420
juliannovoa <julian.novoa@gmail.com>
2021-11-16 17:16:20 +0100

    Added funtion to extract features from dataset. Added tests of function

--- src/ner.py
+++ src/ner.py
@@ -30,3 +30,42 @@ class NameEntityRecognitionModel:
     def check_data(cls, df: pd.DataFrame) -> bool:
         input_columns_set = set(df.columns.values)
         return input_columns_set == NameEntityRecognitionModel.EXPECTED_DATA_COLUMNS
+
+    @classmethod
+    def extract_features(cls, s: pd.Series) -> dict:
+        print(s)
+        word = s['word']
+        postag = s['pos']
+        features = {
+            'word.lower()': word.lower(),  # the word in lowercase
+            'word[-3:]': word[-3:],  # last three characters
+            'word[-2:]': word[-2:],  # last two characters
+            'word.isupper()': word.isupper(),  # true, if the word is in uppercase
+            'word.istitle()': word.istitle(),
+            # true, if the first character is in uppercase and remaining characters are in lowercase
+            'word.isdigit()': word.isdigit(),  # true, if all characters are digits
+            'postag': postag,  # POS tag
+            'postag[:2]': postag[:2],  # IOB prefix
+        }
+
+        for p in ['prev-prev-', 'prev-', 'next-', 'next-next-']:
+            word = s[f'{p}word']  # the neighbors
+            if word.startswith('__START') or word.startswith('__END'):
+                features.update({
+                    f'{p}word:word.lower()': word.lower(),
+                    f'{p}word:word.istitle()': False,
+                    f'{p}word:word.isupper()': False,
+                    f'{p}word:postag': word,
+                    f'{p}word:postag[:2]': word,
+                })
+            else:
+                postag = s[f'{p}pos']  # POS tag of the neighbors
+                features.update({
+                    f'{p}word:word.lower()': word.lower(),
+                    f'{p}word:word.istitle()': word.istitle(),
+                    f'{p}word:word.isupper()': word.isupper(),
+                    f'{p}word:postag': postag,
+                    f'{p}word:postag[:2]': postag[:2],
+                })
+
+        return features
--- src/test/test_ner.py
+++ src/test/test_ner.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 from pathlib import Path
 from unittest import TestCase
@@ -15,7 +16,9 @@ class TestNameEntityRecognitionModel(TestCase):
             f.write(','.join(NameEntityRecognitionModel.EXPECTED_DATA_COLUMNS))
             f.write('\n')
             for line in range(lines):
-                f.write('0,1,2\n')
+                f.write(f'{lines},thousand,of,demonstr,NNS,lowercase,demonstrators,IN,lowercase,Of,NNS,__START1__,'
+                        f'__start1__,ABC,__START2__,__start2__,__START2__,wildcard,__START2__,wildcard,'
+                        f'HI,1,capitalized,2005,O\n')
 
     @classmethod
     def delete_csv(cls):
@@ -90,3 +93,49 @@ class TestNameEntityRecognitionModel(TestCase):
         df = NameEntityRecognitionModel.load_dataset(file)
         self.assertFalse(NameEntityRecognitionModel.check_data(df.assign(new=[1])))
         os.remove(TestNameEntityRecognitionModel.MOCK_FILENAME)
+
+    def test_extract_features(self):
+        expected_features = {
+            'word.lower()': '2005',
+            'word[-3:]': '005',
+            'word[-2:]': '05',
+            'word.isupper()': False,
+            'word.istitle()': False,
+            'word.isdigit()': True,
+            'postag': 'NNS',
+            'postag[:2]': 'NN',
+            'prev-prev-word:word.lower()': '__start2__',
+            'prev-prev-word:word.istitle()': False,
+            'prev-prev-word:word.isupper()': False,
+            'prev-prev-word:postag': '__START2__',
+            'prev-prev-word:postag[:2]': '__START2__',
+            'prev-word:word.lower()': 'hi',
+            'prev-word:word.istitle()': False,
+            'prev-word:word.isupper()': True,
+            'prev-word:postag': 'ABC',
+            'prev-word:postag[:2]': 'AB',
+            'next-word:word.lower()': 'of',
+            'next-word:word.istitle()': True,
+            'next-word:word.isupper()': False,
+            'next-word:postag': 'IN',
+            'next-word:postag[:2]': 'IN',
+            'next-next-word:word.lower()': '__end2__',
+            'next-next-word:word.istitle()': False,
+            'next-next-word:word.isupper()': False,
+            'next-next-word:postag': '__END2__',
+            'next-next-word:postag[:2]': '__END2__',
+
+        }
+        s = pd.Series(data={'word': '2005',
+                            'prev-prev-word': '__START2__',
+                            'prev-word': 'HI',
+                            'next-word': 'Of',
+                            'next-next-word': '__END2__',
+                            'pos': 'NNS',
+                            'prev-prev-pos': '__START2__',
+                            'prev-pos': 'ABC',
+                            'next-pos': 'IN',
+                            'next-next-pos': '__END2__'})
+
+        features = NameEntityRecognitionModel.extract_features(s)
+        self.assertDictEqual(features, expected_features)
